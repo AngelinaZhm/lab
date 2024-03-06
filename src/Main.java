b23fbcc945effa0a51f812a8d9c2b44048c6caa5
Angelina <zhmurkevych447@gmail.com>
2024-03-06 20:27:19 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    int q=51;
+    int w=2;
+    double e = (double) q/w;
+    int r = (int) q%w;
+    System.out.println(e);
+    System.out.println(r);
+
+    int t=34;
+    int u=t/10;
+    int i=t%10;
+    int o=u+i;
+    System.out.println(o);
+
+        double a=43.2;
+        int b= (int) (a+0.5);
+        System.out.println(b);
+    }
+}
